7c23ac648450ef6da022ff5a0faf64cb8e542395
Mikaelly-S <mikaelly_sandy@hotmail.com>
2018-07-04 15:26:26 -0300

    Apps materialize form

--- saa/settings.py
+++ saa/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'material',
+    'material.frontend',
     'usuarios',
     'locais',
     'patrimonios',
     'salas',
     'translados',
-
 ]
 
 MIDDLEWARE = [
